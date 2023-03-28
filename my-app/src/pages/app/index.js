9f97b4b9f5a89a01881ed0bbff152ff37c69e0a1
Candice Bär <candicekbar@gmail.com>
2023-03-28 14:46:46 +0200

    setup: pages and routings

--- my-app/src/pages/app/index.js
+++ my-app/src/pages/app/index.js
@@ -1,24 +1,16 @@
-import logo from '../../assets/imgs/logo.svg';
-import '../../assets/css/app.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from '../login';
+import Dashboard from '../dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Login/>} />
+        <Route exact path='/login' element={<Login/>} />
+        <Route exact path='/dashboard' element={<Dashboard/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ my-app/src/pages/login/index.js
@@ -0,0 +1,25 @@
+import logo from '../../assets/imgs/logo.svg';
+import '../../assets/css/app.css';
+
+function Login() {
+  return (
+    <div className="Login">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Login;
