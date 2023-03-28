3f626a11528b7e334efa524dbfcbdf336ca8e5e6
Candice Bär <candicekbar@gmail.com>
2023-03-28 15:56:18 +0200

    setup: create login form

--- my-app/src/pages/login/index.js
+++ my-app/src/pages/login/index.js
@@ -1,29 +1,63 @@
-import logo from '../../assets/imgs/logo.svg';
-import '../../assets/css/app.css';
-
+import React, { useState } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { actionCreators } from '../../stores/reducers/account';
+import { Col, Row, Form } from 'reactstrap';
+import FormFooter from '../../components/form/footer';
+import FormHeader from '../../components/form/header';
+import FormInput from '../../components/form/input';
 
 
 function Login() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const submitForm = (event) => {
+    console.log("submit >>>", email, password);
+    event.preventDefault();
+  }
+
   return (
-    <div className="Login">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <FormHeader
+        title='Please sign in to your account.'
+      />
+
+      <Form onSubmit={submitForm}>
+        <Row className='form-row'>
+          <Col md={6}>
+            <FormInput
+              id='email'
+              type='email'
+              name='email'
+              placeholder='Email here...'
+              label="Username / Email Address"
+              value={email}
+              onChange={(input) => setEmail(input.target.value)}
+              required
+            />
+          </Col>
+          <Col md={6}>
+            <FormInput
+              type='password'
+              name='password'
+              id='password'
+              placeholder='Password here...'
+              label="Password"
+              value={password}
+              onChange={(input) => setPassword(input.target.value)}
+              required
+            />
+          </Col>
+        </Row>
+
+        <FormFooter
+          action={submitForm}
+          actionText='Sign in'
+        />
+      </Form>
+    </React.Fragment>
   );
 }
 
